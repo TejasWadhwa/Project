d7b32e70d3e5da67100eec4e28e85e7f4a90720d
Anushka <anushkamohal83@gmail.com>
2024-02-23 21:48:17 +0530

    ki

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-const bar = document.getElementById('bar');
-const nav = document.getElementById('navbar');
+// const bar = document.getElementById('bar');
+// const nav = document.getElementById('navbar');
